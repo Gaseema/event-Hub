1c93e42b5c16a13fe4075f6b903ab9b8991562f3
Gaseema <gaseema.n@gmail.com>
2016-02-19 07:24:46 +0300

    event-hub v2

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).foundation();
+$(document).ready(function(){
+  $('.slider4').bxSlider({
+    slideWidth: 300,
+    minSlides: 2,
+    maxSlides: 3,
+    moveSlides: 1,
+    slideMargin: 10
+  });
+});
